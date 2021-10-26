e2c9c4cbaf4ceef6634abdd8991078a83fbd8c40
Josef Kníže <pepatork@gmail.com>
2021-10-26 23:36:57 +0200

    Added Selecting Refernce picture + affine transformation

--- PoseAlignment.py
+++ PoseAlignment.py
@@ -39,6 +39,51 @@ def isRigidTransformation(transformation):
     crossed = np.cross(vectors, np.roll(vectors, 1, axis=0))
     return np.all(crossed[:,2] < 0) or np.all(crossed[:,2] > 0)
 
+def getRansacAffineTransformation(p1, p2, eps = 50):
+    # rearange points in [x1, y1, 1, x2, y2, 1] format 
+    p1 = np.pad(p1, ((0,0),(0,1)), mode='constant', constant_values=1)
+    p2 = np.pad(p2, ((0,0),(0,1)), mode='constant', constant_values=1)
+    stackedPoints = np.hstack((p1, p2))
+    # filter points that openPose didnt find + convert to float32
+    filtered  = np.float32(stackedPoints[np.all(stackedPoints != 0, axis=1)])
+    if filtered.shape[0] < 3:
+        return np.float32([[1,0,0], [0,1,0], [0,0,1]]), { "InliersCount" : -1 }
+
+    # ransac
+    bestInliersCount = -1
+    bestmodel = 0
+    for cycle in range(0, 1000):
+        # chose random 4 rows
+        InlierIndices = np.random.choice(filtered.shape[0], size=3, replace=False)
+        PossibleInliers = filtered[InlierIndices, :]
+
+        # calculate model
+        homography = cv2.getAffineTransform(PossibleInliers[:, [0,1]], PossibleInliers[:, [3,4]])
+        if homography is None:
+            continue    
+        homography = np.vstack((homography,[0,0,1]))
+        homographyTransposed = homography.transpose()
+
+        # filter degenerative homoraphy -- doesn't seem to do anything
+        if not isRigidTransformation(homography):
+            continue
+
+        # find how many points fit the model
+        inliersCount = 0
+        transformed = np.matmul(filtered[:, [0,1,2]], homographyTransposed)
+
+        for transformedPoint, referencePoint in zip(transformed, filtered[:, [3, 4, 5]]):
+            if get2PointsDistance(transformedPoint, referencePoint) < eps:
+                inliersCount += 1
+        if inliersCount > bestInliersCount:
+            bestInliersCount = inliersCount
+            bestmodel = homography
+
+    additionalInfo = {
+        "InliersCount" : bestInliersCount,
+    }
+    return bestmodel, additionalInfo
+
 def getRansacHomography(p1, p2, eps = 50):
     # rearange points in [x1, y1, 1, x2, y2, 1] format 
     p1 = np.pad(p1, ((0,0),(0,1)), mode='constant', constant_values=1)
@@ -76,19 +121,7 @@ def getRansacHomography(p1, p2, eps = 50):
             bestInliersCount = inliersCount
             bestmodel = homography
 
-        # inliersCount = 0
-        # # find how many points fit the model
-        # for (point1, point2) in zip(p1, p2):
-        #     point3d = np.matrix([point1[0], point1[1], 1])
-        #     transformed = np.matmul(point3d, homographyTransposed)[0:1, 0:2]
-        #     distance = get2PointsDistance(transformed, point2)
-        #     if distance < eps:
-        #         inliersCount += 1
-        # if inliersCount > bestInliersCount:
-        #     bestInliersCount = inliersCount
-        #     bestmodel = homography
-        # repeat and find best fit
-
+    # calculate and evaluate final homography
     transformed = np.matmul(filtered[:, [0,1,2]], bestmodel.transpose())
     inliers = filtered[distancesBetweenPoints(transformed, filtered[:, [3,4,5]]) < eps, :]
     
@@ -102,56 +135,75 @@ def getRansacHomography(p1, p2, eps = 50):
     }
     return bestmodel, additionalInfo
 
+
+def AlignImagesInFolder(mainPath):
+    jsonPath = mainPath + "\\Jsons\\"
+    imagesPath = mainPath + "\\Processed\\"
+    alignedPath = mainPath + "\\Aligned\\"
+    # Import Json files, pos_json = position JSON
+    json_files = [pos_json for pos_json in os.listdir(jsonPath) if pos_json.endswith(".json")]
+    print("Found: ", len(json_files), "json keypoint frame files")
+    images = []
+    for file in json_files:
+        keyPoints = json.load(open(jsonPath + file))["people"][0]["pose_keypoints_2d"]
+        points = divideChunks(keyPoints, 3)
+        image = {
+            "path": file,
+            "points": points[:15],
+        }
+        images.append(image)
+
+
+    bestMeanInliers = -1
+    bestRefImage = images[0]
+    # selecting reference picture
+    for refImage in images:
+        SumOfInliers = 0
+        for image in images:
+            homography, homographyInfo = getRansacAffineTransformation(image["points"], refImage["points"])
+            # homography, homographyInfo = getRansacHomography(image["points"], refImage["points"])
+            SumOfInliers += homographyInfo["InliersCount"]
+        if SumOfInliers / (len(images) - 1) > bestMeanInliers:
+            bestRefImage = refImage
+            bestMeanInliers = SumOfInliers / (len(images) - 1)
+
+    print(f"Best ref image is {bestRefImage['path']} with {bestMeanInliers} MeanInliers.")
+
+
+    for image in images:
+        homography, homographyInfo = getRansacAffineTransformation(image["points"], bestRefImage["points"])
+        # homography, homographyInfo = getRansacHomography(image["points"], bestRefImage["points"])
+        imageName = image["path"][:-15]
+        img = cv2.imread(f"{imagesPath}\\{imageName}_rendered.png")
+        out = cv2.warpPerspective(img, homography, (1920, 1080))
+
+        # print info in image
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        org = (5, 20)
+        fontScale = 1
+        color = (255, 0, 0)
+        thickness = 1
+        out = cv2.putText(out, f"Inlier count: {homographyInfo['InliersCount']}",
+        org, font, fontScale, color, thickness, cv2.LINE_AA)
+
+        np.set_printoptions(precision=3)
+        np.set_printoptions(suppress=True)
+
+        out = cv2.putText(out, f"{homography[0, :]}",
+        (5,60), font, fontScale, color, thickness, cv2.LINE_AA)
+
+        out = cv2.putText(out, f"{homography[1, :]}",
+        (5,100), font, fontScale, color, thickness, cv2.LINE_AA)
+
+        out = cv2.putText(out, f"{homography[2, :]}",
+        (5,140), font, fontScale, color, thickness, cv2.LINE_AA)
+
+        # save image
+        cv2.imwrite(f"{alignedPath}{imageName}_aligned.png", out)
+
 ###### MAIN ######
-
-# a = np.array([[1920,1080,1], [-1920,0,1], [1920,-1080,1], [-1920,0,1]])
-# b = np.roll(a,-1,axis=0)
-# c = np.cross(a,b)
-
-main_path = ".\..\data\\Cobra"
-path_to_json = main_path + "\\Jsons\\"
-path_to_images = main_path + "\\Processed\\"
-path_to_aligned = main_path + "\\Aligned\\"
-
-# Import Json files, pos_json = position JSON
-json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith(".json")]
-print("Found: ", len(json_files), "json keypoint frame files")
-images = []
-for file in json_files:
-    keyPoints = json.load(open(path_to_json + file))["people"][0]["pose_keypoints_2d"]
-    points = divideChunks(keyPoints, 3)
-    image = {
-        "path": file,
-        "points": points[:15],
-    }
-    images.append(image)
-
-
-firstname = images[0]["path"][:-15]
-firstImg = cv2.imread(f"{path_to_images}\\{firstname}_rendered.png")
-# vezmu první image a všechny podle něj zarovnám
-for image in images[1:]:
-    homography, homographyInfo = getRansacHomography(image["points"], images[0]["points"])
-    print(image["path"])
-    print(homographyInfo['InliersCount'])
-    imageName = image["path"][:-15]
-    img = cv2.imread(f"{path_to_images}\\{imageName}_rendered.png")
-    out = cv2.warpPerspective(img, homography, (1920, 1080))
-
-
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    org = (5, 25)
-    fontScale = 1
-    color = (255, 0, 0)
-    thickness = 1
-    image = cv2.putText(out, f"Inlier count: {homographyInfo['InliersCount']}",
-     org, font, fontScale, color, thickness, cv2.LINE_AA)
-
-    cv2.imwrite(f"{path_to_aligned}{imageName}_aligned.png", out)
-
-    # cv2.imshow("image window", firstImg)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    # cv2.imshow("image window", out)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+if __name__ == "__main__":
+    dataPath = ".\..\data\\"
+    posePaths = ["Cobra", "DownwardDog", "Child", "Triangle", "WarriorOne", "WarriorTwo", "WarriorTwoBack" ]
+    for path in posePaths:
+        AlignImagesInFolder(dataPath + path)
